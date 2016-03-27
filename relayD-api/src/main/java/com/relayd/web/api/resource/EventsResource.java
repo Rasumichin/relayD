b834f08897109aa4cc461aa8cc47d38dfacb5c41
Erik Lotz <elo@jsmithy.de>
2016-03-27 19:22:17 +0200

    Add further method stubs to get a list of events and a single event.

--- relayD-api/src/main/java/com/relayd/web/api/resource/EventsResource.java
+++ relayD-api/src/main/java/com/relayd/web/api/resource/EventsResource.java
@@ -3,17 +3,29 @@ package com.relayd.web.api.resource;
 import javax.ws.rs.*;
 
 /**
- * @author  Rasumichin (Erik@cloud.franke-net.com)
- * @since   27.03.2016
- * status   initial
+ * @author Rasumichin (Erik@cloud.franke-net.com)
+ * @since 27.03.2016 status initial
  */
 @Path("events")
 public class EventsResource {
+
+    @GET
+    @Produces("text/plain")
+    public String getEvents() {
+        return "list of events";
+    }
 
     @Path("ping")
     @GET
     @Produces("text/plain")
     public String ping() {
         return "Pong response from class " + getClass().getSimpleName() + ".";
+    }
+
+    @Path("{id}")
+    @GET
+    @Produces("text/plain")
+    public String getEvent(@PathParam("id") String id) {
+        return "event with id '" + id + "'.";
     }
 }
