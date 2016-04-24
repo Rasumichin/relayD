f9e18f3eeb4bdf7a8dab01e7b1677f1f25095405
Erik Lotz <elo@jsmithy.de>
2016-04-24 20:58:47 +0200

    Add first test to insert an RelayEventEntity.

--- relayD-entity-model/src/test/java/com/relayd/entity/RelayEventEntityIT.java
+++ relayD-entity-model/src/test/java/com/relayd/entity/RelayEventEntityIT.java
@@ -4,15 +4,55 @@ import static org.junit.Assert.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * @author Rasumichin (Erik@cloud.franke-net.com)
+ * @since 22.04.2016
+ * status initial
+ */
 public class RelayEventEntityIT {
+	private static EntityManagerFactory EMF;
+	private EntityManager entityManager;
 
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		EMF = Persistence.createEntityManagerFactory("dataSource");
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() {
+		EMF.close();
+	}
+	
+	@Before
+	public void setUp() {
+		entityManager = EMF.createEntityManager();
+	}
+	
+	@After
+	public void tearDown() {
+		entityManager.close();
+	}
+	
 	@Test
-	public void test() {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("dataSource");
-		EntityManager em = emf.createEntityManager();
+	public void testInsertRelayEventEntity() {
+		RelayEventEntity sut = new RelayEventEntity.Builder("title").build();
+		EntityTransaction tx = entityManager.getTransaction();
+		
+		tx.begin();
+		entityManager.persist(sut);
+		tx.commit();
+
+		entityManager.clear();
+		RelayEventEntity result = entityManager.find(RelayEventEntity.class, sut.getId());
+		assertEquals("RelayEventEntity could not be found with 'id=" + sut.getId() + "'.", sut.getId(), result.getId());
 	}
 }
