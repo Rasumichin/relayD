4c7bbd548424a00683ce7b7cd355a2d71240841c
Erik Lotz <elo@jsmithy.de>
2016-05-26 19:29:22 +0200

    Well, first 'real' REST call to 'resources/events/ping' incorporated.

--- relayD-web/src/main/java/com/relayd/web/pagebean/BasicView.java
+++ relayD-web/src/main/java/com/relayd/web/pagebean/BasicView.java
@@ -5,20 +5,54 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 
 import com.relayd.client.jaxb.EventDTO;
 
 /**
- * @author schmollc (Christian@cloud.franke-net.com)
+ * @author schmollc (Christian@relayd.de)
+ * @author Rasumichin (Erik@relayd.de)
  * @since 09.05.2016
  * status initial
+ * 
  */
 @ManagedBean
 @ViewScoped
 public class BasicView implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	private String restTargetRoot;
+	private Client restClient = ClientBuilder.newClient();
 
+	Client getRestClient() {
+		return restClient;
+	}
+	
 	public List<EventDTO> getEvents() {
 		return EventDTO.getRandomEvents();
+	}
+
+	public String getEventsPingRequest() {
+		String pathToResource = "resources/events/ping";
+		WebTarget webTarget = getWebTarget(pathToResource);
+		Response response = webTarget.request().get();
+		String result = response.readEntity(String.class);
+		
+		return result;
+	}
+
+	public void setRestTargetRoot(String targetRoot) {
+		restTargetRoot = targetRoot;
+	}
+
+	String getRestTargetRoot() {
+		return restTargetRoot;
+	}
+
+	WebTarget getWebTarget(String pathToResource) {
+		return getRestClient().target(getRestTargetRoot() + pathToResource);
 	}
 }
--- relayD-web/src/test/java/com/relayd/web/pagebean/BasicViewTest.java
+++ relayD-web/src/test/java/com/relayd/web/pagebean/BasicViewTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+
 import org.junit.Test;
 
 import com.relayd.client.jaxb.EventDTO;
@@ -15,13 +18,52 @@ import com.relayd.client.jaxb.EventDTO;
  */
 
 public class BasicViewTest {
+	
+	private BasicView sut = new BasicView();
+
+	@Test
+	public void testGetRestClient() {
+		Client result = sut.getRestClient();
+		assertNotNull("[restClient] is 'null'.", result);
+	}
+	
+	@Test
+	public void testSetRestTargetRoot() {
+		String restTargetRoot = "http://localhost:8080/";
+		sut.setRestTargetRoot(restTargetRoot);
+		
+		String actualRestTargetRoot = sut.getRestTargetRoot();
+		assertEquals("[restTargetRoot] has not been set correctly.", restTargetRoot, actualRestTargetRoot);
+	}
+	
+	@Test
+	public void testGetWebTarget() {
+		String restTargetRoot = "http://localhost:8080/relayD-api/";
+		sut.setRestTargetRoot(restTargetRoot);
+
+		WebTarget webTarget = sut.getWebTarget("resources/events/ping");
+		String expectedUri = "http://localhost:8080/relayD-api/resources/events/ping";
+		String actualUri = webTarget.getUri().toString();
+
+		assertEquals("WebTarget URI is not correct.", expectedUri, actualUri);
+	}
 
 	@Test
 	public void testGetEventsAnswersThreeElements() {
-		BasicView sut = new BasicView();
 		List<EventDTO> result = sut.getEvents();
 		
 		int mockedNumberOfElements = 3;
 		assertEquals("Event list does not contain expected number of elements.", mockedNumberOfElements, result.size());
+	}
+	
+	@Test
+	public void testGetEventsPingRequest() {
+		String restTargetRoot = "http://localhost:8080/relayD-api/";
+		sut.setRestTargetRoot(restTargetRoot);
+
+		String expectedResult = "Pong response from class EventsResource.";
+		String actualResult = sut.getEventsPingRequest();
+
+		assertEquals("Result of ping request to EventsResource does not match.", expectedResult, actualResult);
 	}
 }
