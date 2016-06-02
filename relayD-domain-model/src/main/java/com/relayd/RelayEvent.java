36b2f73f04100e5cb651fdfe0cd4fc55252c54f1
Christian Schmoll <it1f36@retail-sc.com>
2016-06-02 10:33:24 +0200

    impl equals and hashCode.

--- relayD-domain-model/src/main/java/com/relayd/RelayEvent.java
+++ relayD-domain-model/src/main/java/com/relayd/RelayEvent.java
@@ -27,4 +27,42 @@ public class RelayEvent {
 	public Date getEventDate() {
 		return eventDate;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((eventDate == null) ? 0 : eventDate.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		RelayEvent other = (RelayEvent) obj;
+		if (eventDate == null) {
+			if (other.eventDate != null) {
+				return false;
+			}
+		} else if (!eventDate.equals(other.eventDate)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
 }
