7ada157e20f89b8277bc6b2c6a0093d0ecd50821
Christian Schmoll <it1f36@retail-sc.com>
2016-06-24 16:43:28 +0200

    CC - 100% Test coverage.

--- relayD-domain-model/src/main/java/com/relayd/attributes/EventName.java
+++ relayD-domain-model/src/main/java/com/relayd/attributes/EventName.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class EventName implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private String value;
+	String value;
 
 	public EventName(String anEventName) {
 		if (anEventName == null) {
--- relayD-domain-model/src/test/java/com/relayd/attributes/EventNameTest.java
+++ relayD-domain-model/src/test/java/com/relayd/attributes/EventNameTest.java
@@ -1,8 +1,8 @@
 package com.relayd.attributes;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 /**
  * @author schmollc (Christian@relayd.de)
@@ -10,6 +10,97 @@ import static org.junit.Assert.*;
  * status initial
  */
 public class EventNameTest {
+	@Test
+	public void testGetHashCode() {
+		EventName sut = new EventName("Name");
+
+		int hashCode = sut.hashCode();
+
+		assertEquals(2420426, hashCode);
+
+		sut.value = null;
+
+		hashCode = sut.hashCode();
+
+		assertEquals(31, hashCode);
+
+	}
+
+	@Test
+	public void testEqualsWithMyself() {
+		EventName sut = new EventName("Name");
+
+		boolean result = sut.equals(sut);
+
+		assertTrue(result);
+	}
+
+	@Test
+	public void testEqualsWithNull() {
+		EventName sut = new EventName("Name");
+
+		boolean result = sut.equals(null);
+
+		assertFalse(result);
+	}
+
+	@Test
+	public void testEqualsWithNotCompatibleClass() {
+		EventName sut = new EventName("Name");
+
+		boolean result = sut.equals(new String());
+
+		assertFalse(result);
+	}
+
+	@Test
+	public void testEqualsWithDiffrentValues() {
+		EventName sut = new EventName("Name1");
+
+		EventName secondName = new EventName("Name2");
+
+		boolean result = sut.equals(secondName);
+
+		assertFalse(result);
+
+	}
+
+	@Test
+	public void testEqualsWithSameValues() {
+		EventName sut = new EventName("Name");
+
+		EventName secondName = new EventName("Name");
+
+		boolean result = sut.equals(secondName);
+
+		assertTrue(result);
+	}
+
+	@Test
+	public void testEqualsWithUUIDIsNull() {
+		EventName sut = new EventName("dummy");
+		sut.value = null;
+
+		EventName secondName = new EventName("dummy");
+
+		boolean result = sut.equals(secondName);
+
+		assertFalse(result);
+
+	}
+
+	@Test
+	public void testEqualsWithBothUUIDAreNull() {
+		EventName sut = new EventName("dummy");
+		sut.value = null;
+
+		EventName secondName = new EventName("dummy");
+		secondName.value = null;
+
+		boolean result = sut.equals(secondName);
+
+		assertTrue(result);
+	}
 
 	@Test
 	public void testToString() {
