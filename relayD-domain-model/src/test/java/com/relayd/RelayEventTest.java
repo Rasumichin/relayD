7bc17c897c2d268ff5b62da44abcd1fba4d725b9
Christian Schmoll <it1f36@retail-sc.com>
2016-06-24 16:18:20 +0200

    CC - 100% Test coverage.

--- relayD-domain-model/src/test/java/com/relayd/RelayEventTest.java
+++ relayD-domain-model/src/test/java/com/relayd/RelayEventTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.UUID;
 
 import org.junit.Test;
 
@@ -15,9 +16,104 @@ import com.relayd.attributes.EventName;
  * @author Rasumichin (Erik@relayd.de)
  * @since 19.05.2016
  * status initial
- *
  */
 public class RelayEventTest {
+
+	@Test
+	public void testGetHashCode() {
+		RelayEvent sut = new RelayEvent(null, null);
+		UUID uuid = UUID.fromString("53a27b33-a5cb-4997-8eaf-dcf8bd1cb2d2");
+		sut.setUUID(uuid);
+
+		int hashCode = sut.hashCode();
+
+		assertEquals(-975545171, hashCode);
+
+		sut.setUUID(null);
+
+		hashCode = sut.hashCode();
+
+		assertEquals(31, hashCode);
+	}
+
+	@Test
+	public void testEqualsWithMyself() {
+		RelayEvent sut = new RelayEvent(null, null);
+
+		boolean result = sut.equals(sut);
+
+		assertTrue(result);
+	}
+
+	@Test
+	public void testEqualsWithNull() {
+		RelayEvent sut = new RelayEvent(null, null);
+
+		boolean result = sut.equals(null);
+
+		assertFalse(result);
+	}
+
+	@Test
+	public void testEqualsWithNotCompatibleClass() {
+		RelayEvent sut = new RelayEvent(null, null);
+
+		boolean result = sut.equals(new String());
+
+		assertFalse(result);
+	}
+
+	@Test
+	public void testEqualsWithUUIDIsNull() {
+		RelayEvent sut = new RelayEvent(null, null);
+
+		RelayEvent secondEvent = new RelayEvent(null, null);
+		sut.setUUID(null);
+
+		boolean result = sut.equals(secondEvent);
+
+		assertFalse(result);
+
+	}
+
+	@Test
+	public void testEqualsWithBothUUIDAreNull() {
+		RelayEvent sut = new RelayEvent(null, null);
+		sut.setUUID(null);
+
+		RelayEvent secondEvent = new RelayEvent(null, null);
+		secondEvent.setUUID(null);
+
+		boolean result = sut.equals(secondEvent);
+
+		assertTrue(result);
+	}
+
+	@Test
+	public void testName() {
+		RelayEvent sut = new RelayEvent(null, null);
+
+		EventName expected = new EventName("Name");
+
+		sut.setName(expected);
+
+		EventName result = sut.getName();
+
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void testEventDay() {
+		RelayEvent sut = new RelayEvent(null, null);
+
+		EventDay expected = new EventDay(LocalDate.now());
+
+		sut.setEventDay(expected);
+
+		EventDay result = sut.getEventDay();
+
+		assertEquals(expected, result);
+	}
 
 	@Test
 	public void testCreateValidInstance() {
