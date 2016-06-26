fe362cd8b1172ab2922f334e477efacda5b36821
Christian Schmoll <it1f36@retail-sc.com>
2016-06-26 17:06:30 +0200

    #REL-32 add Workflow for Attribute Surename.

---
+++ relayD-web/src/main/java/com/relayd/web/converter/SurenameValueObjectConverter.java
@@ -0,0 +1,28 @@
+package com.relayd.web.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import com.relayd.attributes.Surename;
+
+/**
+ * @author  schmollc (Christian@cloud.franke-net.com)
+ * @since   26.06.2016
+ * status   initial
+ */
+@FacesConverter("com.relayd.web.converter.SurenameValueObjectConverter")
+public class SurenameValueObjectConverter implements Converter {
+
+	@Override
+	public Object getAsObject(@SuppressWarnings("unused") FacesContext facesContext, @SuppressWarnings("unused") UIComponent uiComponent, String value) {
+		Surename name = Surename.newInstance(value);
+		return name;
+	}
+
+	@Override
+	public String getAsString(@SuppressWarnings("unused") FacesContext facesContext, @SuppressWarnings("unused") UIComponent uiComponent, Object value) {
+		return value.toString();
+	}
+}
---
+++ relayD-web/src/test/java/com/relayd/web/converter/SurenameValueObjectConverterTest.java
@@ -0,0 +1,38 @@
+package com.relayd.web.converter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.relayd.attributes.Surename;
+
+/**
+ * @author  schmollc (Christian@cloud.franke-net.com)
+ * @since   26.06.2016
+ * status   initial
+ */
+public class SurenameValueObjectConverterTest {
+	private SurenameValueObjectConverter sut = new SurenameValueObjectConverter();
+
+	private final String name = "Jonas";
+
+	@Test
+	public void testGetAsObject() {
+		Object result = sut.getAsObject(null, null, name);
+
+		assertNotNull("Expected valid instance.", result);
+		assertEquals(Surename.class, result.getClass());
+		Surename surename = (Surename) result;
+		assertEquals("Attribute not correct.", name, surename.toString());
+	}
+
+	@Test
+	public void testGetAsString() {
+		Surename surename = Surename.newInstance(name);
+
+		String result = sut.getAsString(null, null, surename);
+
+		assertNotNull("Expected valid instance.", result);
+		assertEquals("Attribute not correct.", name, result);
+	}
+}
