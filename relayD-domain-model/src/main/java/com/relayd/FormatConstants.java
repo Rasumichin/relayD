4c401cac5c3a2e001764dd82fbe431694444b2b5
Christian Schmoll <it1f36@retail-sc.com>
2016-07-15 08:01:14 +0200

    Introduce FormatConstants in domain-model.

---
+++ relayD-domain-model/src/main/java/com/relayd/FormatConstants.java
@@ -0,0 +1,17 @@
+package com.relayd;
+
+/**
+ * Bloch, Joshua, Effective Java, 2nd Edition, Item 19, p. 98
+ *
+ * @author schmollc (Christian@relayd.de)
+ * @since 27.06.2016
+ * status initial
+ */
+public final class FormatConstants {
+
+	private FormatConstants() {
+		//	restrict instantiation
+	}
+
+	public static final String DATE_FORMAT = "yyyy-MM-dd";
+}
