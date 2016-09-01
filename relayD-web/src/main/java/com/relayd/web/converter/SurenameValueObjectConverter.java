be0f7e9ed76077a3a5b98b8210bff82f77c7c2ee
Erik Lotz <elo@jsmithy.de>
2016-09-01 22:01:17 +0200

    REL-60 Refactoring for type 'Surename'.

--- relayD-web/src/main/java/com/relayd/web/converter/SurenameValueObjectConverter.java
+++ relayD-web/src/main/java/com/relayd/web/converter/SurenameValueObjectConverter.java
@@ -1,28 +1,21 @@
 package com.relayd.web.converter;
 
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
 import com.relayd.attributes.Surename;
 
 /**
  * @author  schmollc (Christian@relayD.de)
+ * @author  Rasumichin (Erik@relayd.de)
  * @since   26.06.2016
  * status   initial
+ * 
  */
 @FacesConverter("com.relayd.web.converter.SurenameValueObjectConverter")
-public class SurenameValueObjectConverter implements Converter {
+public class SurenameValueObjectConverter extends NameValueObjectConverter {
 
 	@Override
-	public Object getAsObject(@SuppressWarnings("unused") FacesContext facesContext, @SuppressWarnings("unused") UIComponent uiComponent, String value) {
-		Surename name = Surename.newInstance(value);
-		return name;
-	}
-
-	@Override
-	public String getAsString(@SuppressWarnings("unused") FacesContext facesContext, @SuppressWarnings("unused") UIComponent uiComponent, Object value) {
-		return value.toString();
+	Surename getName(String nameValue) {
+		return Surename.newInstance(nameValue);
 	}
 }
