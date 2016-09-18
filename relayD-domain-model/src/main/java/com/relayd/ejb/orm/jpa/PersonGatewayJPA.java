becb01e90184c004dd27a56c35a9a0596fce4fe2
Erik Lotz <elo@jsmithy.de>
2016-09-18 12:07:00 +0200

    REL-85 Work in progress: start implementing gateway services.

--- relayD-domain-model/src/main/java/com/relayd/ejb/orm/jpa/PersonGatewayJPA.java
+++ relayD-domain-model/src/main/java/com/relayd/ejb/orm/jpa/PersonGatewayJPA.java
@@ -1,25 +1,37 @@
 package com.relayd.ejb.orm.jpa;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+
+import javax.persistence.*;
 
 import com.relayd.Person;
 import com.relayd.ejb.PersonGateway;
+import com.relayd.entity.PersonEntity;
 
 /**
  * @author  schmollc (Christian@relayd.de)
+ * @author  Rasumichin (Erik@relayd.de)
  * @since   12.09.2016
  *
  */
 public class PersonGatewayJPA implements PersonGateway {
+	private static EntityManagerFactory EM_FACTORY = Persistence.createEntityManagerFactory("dataSource");
 
 	@Override
 	public List<Person> getAll() {
-		return null;
+		EntityManager em = EM_FACTORY.createEntityManager();
+		List<PersonEntity> result = em.createQuery("SELECT p FROM PersonEntity p", PersonEntity.class).getResultList();
+		System.out.println("Result getAll: " + result.size());
+		
+		return new ArrayList<>();
 	}
 
 	@Override
 	public Person get(UUID uuid) {
+		EntityManager em = EM_FACTORY.createEntityManager();
+		PersonEntity result = em.find(PersonEntity.class, uuid.toString());
+		System.out.println("Result get: " + result);
+		
 		return null;
 	}
 
