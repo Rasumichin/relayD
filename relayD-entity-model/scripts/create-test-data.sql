2e68964301c577579db8fdab398061803dc4dcb1
Christian Schmoll <it1f36@retail-sc.com>
2016-09-22 21:40:38 +0200

    REL-62 - Anlage der YearOfBirth Klasse.

--- relayD-entity-model/scripts/create-test-data.sql
+++ relayD-entity-model/scripts/create-test-data.sql
@@ -4,5 +4,5 @@ insert into RELAY_EVENT (id, eventName, eventDay, updateUser) values ("295a785f-
 insert into RELAY_EVENT (id, eventName, eventDay, updateUser) values ("ca58e093-64d3-4971-b08c-e7cd6d16e469", "New York Marathon", "2017-10-07", "script");
 insert into RELAY_EVENT (id, eventName, eventDay, updateUser) values ("85d7094b-2146-4f52-8bd9-901e71723f31", "Kö-Lauf Düsseldorf", "2018-08-31", "script");
 --Entity Person
-insert into person(id,surename,forename,birthyear,shirtsize,relayname,pos,nationality,email,info) values("89d7134b-2326-4f52-7bd7-901e71723f31", "Jonas", "Justus", 1971, 1, "Die 4 ????", 1, "DE", "Justus.Jonas@RockyBeach.com", null);
-insert into person(id,surename,forename,birthyear,shirtsize,relayname,pos,nationality,email,info) values("90d71de4-bc13-4f52-7bd7-901b71723c22", "Peter", "Shaw", 1972, 1, "Die 4 ????", 1, "DE", "Peter.Shaw@RockyBeach.com", null);
+insert into person(id,surename,forename,yearOfBirth,shirtsize,relayname,pos,nationality,email,info) values("89d7134b-2326-4f52-7bd7-901e71723f31", "Jonas", "Justus", 1971, 1, "Die 4 ????", 1, "DE", "Justus.Jonas@RockyBeach.com", null);
+insert into person(id,surename,forename,yearOfBirth,shirtsize,relayname,pos,nationality,email,info) values("90d71de4-bc13-4f52-7bd7-901b71723c22", "Peter", "Shaw", 1972, 1, "Die 4 ????", 1, "DE", "Peter.Shaw@RockyBeach.com", null);
