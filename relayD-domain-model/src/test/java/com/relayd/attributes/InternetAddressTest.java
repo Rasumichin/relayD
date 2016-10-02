89026139d63dc0299ea1fe2bec265b49f2c7361e
Erik Lotz <elo@jsmithy.de>
2016-10-02 13:50:08 +0200

    REL-92 Refactoring and continue exploring InternetAddress.

--- relayD-domain-model/src/test/java/com/relayd/attributes/InternetAddressTest.java
+++ relayD-domain-model/src/test/java/com/relayd/attributes/InternetAddressTest.java
@@ -8,47 +8,63 @@ import javax.mail.internet.*;
 
 import org.junit.Test;
 
+/**
+ * Used this test class to get familiar with the class under test: javax.mail.internet.InternetAddress.
+ * 
+ * @author  Rasumichin (Erik@relayd.de)
+ * @since   01.10.2016
+ * 
+ */
 public class InternetAddressTest {
 
 	@Test
-	public void testCreateInternetAddressFromString() throws AddressException {
+	public void testCreateInstance_usualAddress() throws AddressException {
 		String expected = "peter.pan@mail.com";
-		InternetAddress sut = new InternetAddress(expected);
+		InternetAddress sut = new InternetAddress(expected, true);
 		
 		String result = sut.getAddress();
 		assertEquals("Constructur does not work as expected.", expected, result);
 	}
 	
+	@Test
+	public void testCreateInstance_localPartContainsTwoDots() throws AddressException {
+		String expected = "this..IsCorrect@mail.com";
+		InternetAddress sut = new InternetAddress(expected, true);
+
+		String result = sut.getAddress();
+		assertEquals("Constructur does not work as expected.", expected, result);
+	}
+	
 	@Test(expected=AddressException.class)
-	public void testCreateFalseInternetAddress_noAtSign() throws AddressException {
+	public void testCreateFalseInstance_noAtSign() throws AddressException {
 		String mail = "peter.pan";
 		@SuppressWarnings("unused")
 		InternetAddress sut = new InternetAddress(mail, true);
 	}
 	
 	@Test(expected=AddressException.class)
-	public void testCreateFalseInternetAddress_containsWhitespace() throws AddressException {
+	public void testCreateFalseInstance_containsWhitespace() throws AddressException {
 		String mail = "Robert van Gember@canda.com";
 		@SuppressWarnings("unused")
 		InternetAddress sut = new InternetAddress(mail, true);
 	}
 	
 	@Test(expected=AddressException.class)
-	public void testCreateFalseInternetAddress_containsWhitespaceOnly() throws AddressException {
+	public void testCreateFalseInstance_containsWhitespaceOnly() throws AddressException {
 		String mail = "  ";
 		@SuppressWarnings("unused")
 		InternetAddress sut = new InternetAddress(mail, true);
 	}
 	
 	@Test(expected=AddressException.class)
-	public void testCreateFalseInternetAddress_emptyString() throws AddressException {
+	public void testCreateFalseInstance_emptyString() throws AddressException {
 		String mail = "";
 		@SuppressWarnings("unused")
 		InternetAddress sut = new InternetAddress(mail, true);
 	}
 	
 	@Test(expected=NullPointerException.class)
-	public void testCreateFalseInternetAddress_null() throws AddressException {
+	public void testCreateFalseInstance_nullValue() throws AddressException {
 		String mail = null;
 		@SuppressWarnings("unused")
 		InternetAddress sut = new InternetAddress(mail, true);
