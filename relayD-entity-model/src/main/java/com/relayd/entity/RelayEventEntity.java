857cc31049b7a525e3d59fb04953fa568f3dd791
Christian Schmoll <it1f36@retail-sc.com>
2016-10-04 20:43:56 +0200

    REL-100 - Refactor Entity.

--- relayD-entity-model/src/main/java/com/relayd/entity/RelayEventEntity.java
+++ relayD-entity-model/src/main/java/com/relayd/entity/RelayEventEntity.java
@@ -3,46 +3,48 @@ package com.relayd.entity;
 import java.sql.Date;
 import java.util.UUID;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * JPA Entity class to represent the underlying RDBMS table which persists the 'Event' data.
  * Applies Joshua Bloch's 'Builder Pattern' to create instances programmatically.
- * 
+ *
  * @author Rasumichin (Erik@relayd.de)
  * @since 22.04.2016
- * status initial
- * 
+ *
  */
 @Entity
-@Table(name="RELAY_EVENT")
+@Table(name = "relay_event")
 public class RelayEventEntity {
-	
+
 	@Id
-	@Column(nullable=false, length=36)
+	@Column
 	private String id;
-	
-	@Column(nullable=false, length=64)
+
+	@Column
 	private String eventName;
-	
-	@Column(nullable=false)
+
+	@Column
 	private Date eventDay;
-	
+
 	public static class Builder {
 		private final String id = UUID.randomUUID().toString();
 		private final String eventName;
 		private Date eventDay = new Date(System.currentTimeMillis());
-		
+
 		public Builder(String anEventName) {
 			if (null == anEventName) {
 				throw new IllegalArgumentException("'[anEventName] must not be 'null'.");
 			}
 			eventName = anEventName;
 		}
-		
+
 		public Builder withEventDay(Date anEventDay) {
 			validateEventDay(anEventDay);
-			
+
 			eventDay = anEventDay;
 			return this;
 		}
@@ -57,7 +59,7 @@ public class RelayEventEntity {
 			return new RelayEventEntity(this);
 		}
 	}
-	
+
 	private RelayEventEntity(Builder aBuilder) {
 		id = aBuilder.id;
 		eventName = aBuilder.eventName;
@@ -67,15 +69,15 @@ public class RelayEventEntity {
 	/**
 	 * Provided empty constructor to conform to the JPA spec. For that reason the entity property fields
 	 * are not declared 'final'.
-	 * 
+	 *
 	 */
 	protected RelayEventEntity() {
 	}
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public String getEventName() {
 		return eventName;
 	}
