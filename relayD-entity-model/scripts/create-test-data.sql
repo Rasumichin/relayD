76d9f81ad5a8f5b6b6b8d563661a50fa2363fbd8
Christian Schmoll <it1f36@retail-sc.com>
2016-10-04 20:38:54 +0200

    REL-100 - Create sql-scripts for DB-table.

--- relayD-entity-model/scripts/create-test-data.sql
+++ relayD-entity-model/scripts/create-test-data.sql
@@ -1,8 +1,6 @@
--- Entitiy Relay
-insert into RELAY_EVENT (id, eventName, eventDay, updateUser) values ("5697d710-8967-4b2d-9ab2-8fc50ddc6138", "Metro Marathon Düsseldorf", "2017-04-29", "script");
-insert into RELAY_EVENT (id, eventName, eventDay, updateUser) values ("295a785f-b421-4fd2-9bc8-222ce7d5c0ab", "Boston Marathon", "2017-06-11", "script");
-insert into RELAY_EVENT (id, eventName, eventDay, updateUser) values ("ca58e093-64d3-4971-b08c-e7cd6d16e469", "New York Marathon", "2017-10-07", "script");
-insert into RELAY_EVENT (id, eventName, eventDay, updateUser) values ("85d7094b-2146-4f52-8bd9-901e71723f31", "Kö-Lauf Düsseldorf", "2018-08-31", "script");
---Entity Person
-insert into person(id,surename,forename,yearOfBirth,shirtsize,relayname,pos,nationality,email,info) values("89d7134b-2326-4f52-7bd7-901e71723f31", "Jonas", "Justus", 1971, 1, "Die 4 ????", 1, "DE", "Justus.Jonas@RockyBeach.com", null);
-insert into person(id,surename,forename,yearOfBirth,shirtsize,relayname,pos,nationality,email,info) values("90d71de4-bc13-4f52-7bd7-901b71723c22", "Peter", "Shaw", 1972, 1, "Die 4 ????", 1, "DE", "Peter.Shaw@RockyBeach.com", null);
+-- Entity relay_event
+insert into relay_event (id, eventName, eventDay) values ("5697d710-8967-4b2d-9ab2-8fc50ddc6138", "Metro Group Marathon Düsseldorf", "2017-04-30");
+
+-- Entity person
+insert into person(id, surename, forename, yearOfBirth, shirtsize, relayname, pos, nationality, email, info) values("89d7134b-2326-4f52-7bd7-901e71723f31", "Jonas", "Justus", 1971, 1, "Die 4 ????", 1, "DE", "Justus.Jonas@RockyBeach.com", null);
+insert into person(id, surename, forename, yearOfBirth, shirtsize, relayname, pos, nationality, email, info) values("90d71de4-bc13-4f52-7bd7-901b71723c22", "Peter", "Shaw", 1972, 1, "Die 4 ????", 1, "DE", "Peter.Shaw@RockyBeach.com", null);
