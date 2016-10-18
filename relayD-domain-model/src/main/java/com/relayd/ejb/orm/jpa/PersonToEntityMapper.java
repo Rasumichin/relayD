2101f65948e38abdff6c1130f93c228c1f6a6dcc
Erik Lotz <elo@jsmithy.de>
2016-10-18 16:39:48 +0200

    REL-122 Refactoring. Remove 'mapPersonToEntity(Person)'.

--- relayD-domain-model/src/main/java/com/relayd/ejb/orm/jpa/PersonToEntityMapper.java
+++ relayD-domain-model/src/main/java/com/relayd/ejb/orm/jpa/PersonToEntityMapper.java
@@ -28,21 +28,6 @@ public class PersonToEntityMapper {
 		return new PersonToEntityMapper();
 	}
 
-	public PersonEntity mapPersonToEntity(Person person) {
-		PersonEntity result = new PersonEntity.Builder().withId(person.getUUID().toString())
-				.withForename((person.getForename() == null) || (person.getForename().isEmpty()) ? null : person.getForename().toString())
-				.withSurename((person.getSurename() == null) ? null : person.getSurename().toString())
-				.withEmail((person.getEmail() == null) ? null : person.getEmail().toString())
-				.withYearOfBirth((person.getYearOfBirth() == null) ? null : person.getYearOfBirth().getValue())
-				.withComment((person.getComment() == null) ? null : person.getComment().toString())
-				.withNationality((person.getNationality() == null) ? null : person.getNationality().toLanguageTag())
-				.withRelayname((person.getRelayname() == null) ? null : person.getRelayname().toString())
-				.withPos((person.getPosition() == null) ? null : person.getPosition().getValue())
-				.withShirtsize((person.getShirtsize() == null) ? null : person.getShirtsize().getSize().intValue()).build();
-
-		return result;
-	}
-
 	public void mapPersonToExistingEntity(Person person, PersonEntity personEntity) {
 		personEntity.setForename((person.getForename() == null) || (person.getForename().isEmpty()) ? null : person.getForename().toString());
 		personEntity.setSurename((person.getSurename() == null) ? null : person.getSurename().toString());
