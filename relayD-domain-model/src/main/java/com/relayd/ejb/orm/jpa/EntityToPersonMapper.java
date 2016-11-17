33a57560cdac73544b26c732350ba66e0eadb66c
Christian Schmoll <it1f36@retail-sc.com>
2016-11-17 19:48:49 +0100

    Use the glory of NOP.

--- relayD-domain-model/src/main/java/com/relayd/ejb/orm/jpa/EntityToPersonMapper.java
+++ relayD-domain-model/src/main/java/com/relayd/ejb/orm/jpa/EntityToPersonMapper.java
@@ -3,7 +3,14 @@ package com.relayd.ejb.orm.jpa;
 import java.util.UUID;
 
 import com.relayd.Person;
-import com.relayd.attributes.*;
+import com.relayd.attributes.Comment;
+import com.relayd.attributes.Email;
+import com.relayd.attributes.Forename;
+import com.relayd.attributes.Position;
+import com.relayd.attributes.Relayname;
+import com.relayd.attributes.Shirtsize;
+import com.relayd.attributes.Surename;
+import com.relayd.attributes.YearOfBirth;
 import com.relayd.entity.PersonEntity;
 
 /**
@@ -24,20 +31,20 @@ public class EntityToPersonMapper {
 		if (personEntity == null) {
 			throw new IllegalArgumentException("[personEntity] must not be 'null'.");
 		}
-		
+
 		Person person = Person.newInstance();
 		person.setUuid(UUID.fromString(personEntity.getId()));
-		person.setComment((personEntity.getComment() == null) ? null : (Comment.newInstance(personEntity.getComment())));
-		person.setEmail((personEntity.getEmail() == null) ? person.getEmail() : (Email.newInstance(personEntity.getEmail())));
 
 		// Thank god for the 'NullObjectPattern'.
 		person.setForename(Forename.newInstance(personEntity.getForename()));
 		person.setYearOfBirth(YearOfBirth.newInstance(personEntity.getYearOfBirth()));
-		
-		person.setPosition((personEntity.getPos() == null) ? null : (Position.decode(personEntity.getPos())));
-		person.setRelayname((personEntity.getRelayname() == null) ? null : (Relayname.newInstance(personEntity.getRelayname())));
-		person.setShirtsize((personEntity.getShirtsize() == null) ? null : (Shirtsize.decode(personEntity.getShirtsize())));
-		person.setSurename((personEntity.getSurename() == null) ? null : (Surename.newInstance(personEntity.getSurename())));
+		person.setRelayname(Relayname.newInstance(personEntity.getRelayname()));
+		person.setComment(Comment.newInstance(personEntity.getComment()));
+
+		person.setEmail((personEntity.getEmail() == null) ? person.getEmail() : Email.newInstance(personEntity.getEmail()));
+		person.setPosition((personEntity.getPos() == null) ? null : Position.decode(personEntity.getPos()));
+		person.setShirtsize((personEntity.getShirtsize() == null) ? null : Shirtsize.decode(personEntity.getShirtsize()));
+		person.setSurename((personEntity.getSurename() == null) ? null : Surename.newInstance(personEntity.getSurename()));
 
 		return person;
 	}
