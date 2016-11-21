1869d5c03fb052c0b208492f2eb3ae7585c532cf
Christian Schmoll <it1f36@retail-sc.com>
2016-11-21 19:48:37 +0100

    rel-159 - RelayGatewayFile angelegt.

--- relayD-domain-model/src/test/java/com/relayd/ejb/RelayGatewayTest.java
+++ relayD-domain-model/src/test/java/com/relayd/ejb/RelayGatewayTest.java
@@ -55,43 +55,30 @@ public abstract class RelayGatewayTest {
 		assertFalse("The List of relays should contain data!", actual);
 	}
 
-	//	@Test
-	//	public void testSet_ForExistingRelay() {
-	//		// ARRANGE
-	//		Relay firstRelay = Relay.newInstance();
-	//		firstRelay.setRelayname(Relayname.newInstance("Staffel 1"));
-	//		UUID uuidFromFirstMember = firstRelay.getUuid();
-	//
-	//		getSut().set(firstRelay);
-	//		getSut().set(createRelayStaubwolke());
-	//
-	//		Person updateMember = getSut().get(uuidFromFirstMember);
-	//
-	//		Forename newForename = Forename.newInstance("Bob");
-	//		updateMember.setForename(newForename);
-	//		Surename newSurename = Surename.newInstance("Andrews");
-	//		updateMember.setSurename(newSurename);
-	//		YearOfBirth newYearOfBirth = YearOfBirth.newInstance(1971);
-	//		updateMember.setYearOfBirth(newYearOfBirth);
-	//		Shirtsize newShirtsize = Shirtsize.DamenXS;
-	//		updateMember.setShirtsize(newShirtsize);
-	//		Email newEmail = Email.newInstance("Bob.Andrews@rockyBeach.com");
-	//		updateMember.setEmail(newEmail);
-	//		// ACT
-	//		getSut().set(updateMember);
-	//
-	//		// ASSERT
-	//		Person checkPerson = getSut().get(uuidFromFirstMember);
-	//		assertNotNull(checkPerson);
-	//
-	//		assertEquals("[Forename] not correct.", newForename, checkPerson.getForename());
-	//		assertEquals("[Surename] not correct.", newSurename, checkPerson.getSurename());
-	//		assertEquals("[YearOfBirth] not correct.", newYearOfBirth, checkPerson.getYearOfBirth());
-	//		assertEquals("[Shirtsize] not correct.", newShirtsize, checkPerson.getShirtsize());
-	//		assertEquals("[Email] not correct.", newEmail, checkPerson.getEmail());
-	//
-	//	}
-	//
+	@Test
+	public void testSet_ForExistingRelay() {
+		// ARRANGE
+		Relay firstRelay = Relay.newInstance();
+		firstRelay.setRelayname(Relayname.newInstance("Staffel 1"));
+		UUID uuidFromFirstMember = firstRelay.getUuid();
+
+		getSut().set(firstRelay);
+		getSut().set(createRelayStaubwolke());
+
+		Relay updateRelay = getSut().get(uuidFromFirstMember);
+
+		Relayname newRelayname = Relayname.newInstance("Wuestenwind");
+		updateRelay.setRelayname(newRelayname);
+
+		// ACT
+		getSut().set(updateRelay);
+
+		// ASSERT
+		Relay checkRelay = getSut().get(uuidFromFirstMember);
+		assertNotNull(checkRelay);
+		assertEquals("[Relayname] not correct.", newRelayname, checkRelay.getRelayname());
+	}
+
 	private Relay createRelayStaubwolke() {
 		Relay relay = Relay.newInstance();
 		relay.setRelayname(Relayname.newInstance("Staubwolke"));
