ee60a5506e690364aff96935a17e4d2388163bd4
Christian Schmoll <it1f36@retail-sc.com>
2016-11-25 12:47:13 +0100

    rel-153 - create Builder.

---
+++ relayD-domain-model/src/test/java/com/relayd/ParticipantBuilder.java
@@ -0,0 +1,51 @@
+package com.relayd;
+
+import java.util.UUID;
+
+import com.relayd.attributes.Forename;
+import com.relayd.attributes.Surename;
+
+/**
+ * @author  schmollc (Christian@relayd.de)
+ * @since   25.11.2016
+ *
+ */
+public class ParticipantBuilder {
+
+	private UUID uuidPerson = UUID.fromString("89d7134b-2326-4f52-7bd7-901e71723f31");
+	private Surename surename = Surename.newInstance("Surename");
+	private Forename forename = Forename.newInstance("Forename");
+
+	public ParticipantBuilder withSurename(Surename aSurename) {
+		surename = aSurename;
+		return this;
+	}
+
+	public ParticipantBuilder withSurename(String aSurename) {
+		surename = Surename.newInstance(aSurename);
+		return this;
+	}
+
+	public ParticipantBuilder withForename(Forename aForename) {
+		forename = aForename;
+		return this;
+	}
+
+	public ParticipantBuilder withForename(String aForename) {
+		forename = Forename.newInstance(aForename);
+		return this;
+	}
+
+	public ParticipantBuilder withUUIDPerson(UUID anUUIDPerson) {
+		uuidPerson = anUUIDPerson;
+		return this;
+	}
+
+	public Participant build() {
+		Participant participant = Participant.newInstance();
+		participant.setUuidPerson(uuidPerson);
+		participant.setSurename(surename);
+		participant.setForename(forename);
+		return participant;
+	}
+}
