030c5f4c6bb08e189ba727d82cf29269871605b2
Christian Schmoll <it1f36@retail-sc.com>
2016-11-25 13:04:56 +0100

    rel-153 - make relay persistant.

--- relayD-domain-model/src/main/java/com/relayd/ejb/orm/jpa/RelayGatewayJPA.java
+++ relayD-domain-model/src/main/java/com/relayd/ejb/orm/jpa/RelayGatewayJPA.java
@@ -3,15 +3,59 @@ package com.relayd.ejb.orm.jpa;
 import java.util.List;
 import java.util.UUID;
 
+import javax.persistence.EntityManager;
+
 import com.relayd.Relay;
 import com.relayd.ejb.RelayGateway;
+import com.relayd.entity.RelayEntity;
 
 /**
  * @author  schmollc (Christian@relayd.de)
  * @since   19.11.2016
  *
  */
-public class RelayGatewayJPA implements RelayGateway {
+public class RelayGatewayJPA extends GatewayJPA implements RelayGateway {
+	private RelayToEntityMapper relayMapper = RelayToEntityMapper.newInstance();
+
+	@Override
+	public void set(Relay relay) {
+		if (relay == null) {
+			throw new IllegalArgumentException("[relay] must not be 'null'.");
+		}
+
+		RelayEntity relayEntity = getRelayEntitiy(relay);
+
+		getRelayMapper().mapRelayToEntity(relay, relayEntity);
+
+		mergePersonEntity(relayEntity);
+	}
+
+	private RelayEntity getRelayEntitiy(Relay relay) {
+		RelayEntity relayEntity = findById(relay.getUuid());
+		if (relayEntity == null) {
+			relayEntity = RelayEntity.newInstance(relay.getUuid());
+		}
+		return relayEntity;
+	}
+
+	RelayEntity findById(UUID uuid) {
+		EntityManager em = getEntityManager();
+		RelayEntity result = em.find(RelayEntity.class, uuid.toString());
+
+		return result;
+	}
+
+	// TODO Gleicher Code wie in der PersonGatewayJPA Klasse
+	void mergePersonEntity(RelayEntity relayEntity) {
+		startTransaction();
+		getEntityManager().merge(relayEntity);
+		commitTransaction();
+		endTransaction();
+	}
+
+	private RelayToEntityMapper getRelayMapper() {
+		return relayMapper;
+	}
 
 	@Override
 	public List<Relay> getAll() {
@@ -19,12 +63,7 @@ public class RelayGatewayJPA implements RelayGateway {
 	}
 
 	@Override
-	public void set(Relay aRelay) {
-		throw new UnsupportedOperationException("not implemented yet!");
-	}
-
-	@Override
-	public Relay get(UUID aUuid) {
+	public Relay get(UUID uuid) {
 		throw new UnsupportedOperationException("not implemented yet!");
 	}
 }
---
+++ relayD-domain-model/src/test/java/com/relayd/ejb/orm/jpa/RelayGatewayJPATest.java
@@ -0,0 +1,26 @@
+package com.relayd.ejb.orm.jpa;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Write your code so that it reflects, or rises above, the best parts of your personal character.
+ *  - Daniel Read
+ *
+ * @author  schmollc (Christian@relayd.de)
+ * @since   25.11.2016
+ *
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(MockitoJUnitRunner.class)
+public class RelayGatewayJPATest {
+
+	private RelayGatewayJPA sut = new RelayGatewayJPA();
+
+	@Test
+	public void testSet_ForExistingRelay() {
+	}
+}
