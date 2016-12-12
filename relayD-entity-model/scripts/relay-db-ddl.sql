0e8fb3513de0390f7459d1dcc419ca05537c48f9
Erik Lotz <elo@jsmithy.de>
2016-12-12 21:36:11 +0100

    REL-190 Add 2nd edition of the 'relay' table.
    This one does not include a static number of participant references.
    Moreover, correct the syntax of DDL statements (regarding comments).

--- relayD-entity-model/scripts/relay-db-ddl.sql
+++ relayD-entity-model/scripts/relay-db-ddl.sql
@@ -41,3 +41,44 @@ ALTER TABLE relay ADD PRIMARY KEY (id);
 -- Grant relay
 --------------------------------------------------
 GRANT ALL ON relay TO PUBLIC; -- geht nicht!
+
+-- Alternative edition of table 'relay'
+-- ------------------------------------------------
+-- Drop Table relay2
+-- ------------------------------------------------
+DROP TABLE relay2;
+
+-- ------------------------------------------------
+-- Create Table relay2
+-- ------------------------------------------------
+CREATE TABLE relay2 (
+	id								CHAR(36)		NOT NULL	COMMENT 'UUID as string representation.',
+	eventId							CHAR(36) 		NOT NULL	COMMENT 'UUID from the Event string representation.',
+	relayname						VARCHAR(256)	NOT NULL	COMMENT 'The name of the relay.'
+) COMMENT 'relayD - The table for the relay entities.';
+
+
+-- ------------------------------------------------
+-- Create Index relay2_IDX
+-- ------------------------------------------------
+CREATE UNIQUE INDEX relay2_idx
+        ON relay2
+        (id);
+
+-- ------------------------------------------------
+-- Create Primary Key
+-- ------------------------------------------------
+ALTER TABLE relay2 ADD PRIMARY KEY (id);
+
+-- ------------------------------------------------
+-- Create Index relay2_IDX_EVENT_RELAY
+-- ------------------------------------------------
+-- CREATE UNIQUE INDEX relay2_IDX_EVENT_RELAY
+--         ON relay2
+--         (eventId, relayname);
+-- Index cannot be created since the combination of both columns exceeds the maximum index bytes length.
+
+-- ------------------------------------------------
+-- Grant relay
+-- ------------------------------------------------
+GRANT ALL ON relay2 TO PUBLIC; -- geht nicht!
