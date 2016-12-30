e9731175c87b8cab803ffc4688a358e30e11fded
Christian Schmoll <it1f36@retail-sc.com>
2016-12-30 17:34:30 +0100

    #REL-193 - remove todo.

--- relayD-web/src/test/java/com/relayd/web/bridge/TreeNodeRowTest.java
+++ relayD-web/src/test/java/com/relayd/web/bridge/TreeNodeRowTest.java
@@ -8,8 +8,9 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import com.relayd.Participant;
 import com.relayd.Relay;
-import com.relayd.RelayEvent;
+import com.relayd.attributes.Position;
 
 /**
  * The time to write good code is at the time you are writing it.
@@ -21,18 +22,69 @@ import com.relayd.RelayEvent;
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TreeNodeRowTest {
-	// TODO (Christian, Version 1.4): Wie teste ich diese Klasse die ja eigentlich abstract ist!
-	// Test ist nur drin damit JUnit nicht motzt!
+	private TreeNodeRow sut = new TreeNodeRow() {
+		private static final long serialVersionUID = 1L;
+	};
 
 	@Test
 	public void testIsSerializable() {
-		Relay dummyRelay = Relay.newInstance(RelayEvent.duesseldorf());
-
-		TreeNodeRow sut = TreeNodeRow.newInstance(dummyRelay);
-
 		@SuppressWarnings("cast")
-		boolean result = sut instanceof Serializable;
+		boolean condition = sut instanceof Serializable;
 
-		assertTrue("Class not Serializable!", result);
+		assertTrue("Class not Serializable!", condition);
+	}
+
+	@Test
+	public void testNewInstance_ForTreeNodeRowRelay() {
+		Relay relay = Relay.newInstance();
+
+		TreeNodeRow sutForTreeNodeRowRelay = TreeNodeRow.newInstance(relay);
+
+		boolean condition = sutForTreeNodeRowRelay instanceof TreeNodeRowRelay;
+
+		assertTrue("Class not correct instance!", condition);
+	}
+
+	@Test
+	public void testNewInstance_ForTreeNodeRowParticipant() {
+		Participant participant = Participant.newInstance();
+
+		TreeNodeRow sutForTreeNodeRowRelay = TreeNodeRow.newInstance(participant, Position.FIRST);
+
+		boolean condition = sutForTreeNodeRowRelay instanceof TreeNodeRowParticipant;
+
+		assertTrue("Class not correct instance!", condition);
+	}
+
+	@Test
+	public void testIsRelay() {
+		boolean condition = sut.isRelay();
+
+		assertFalse("[isRelay] not correct", condition);
+	}
+
+	@Test
+	public void testGetRelayname() {
+		String actual = sut.getRelayname();
+
+		String expected = "";
+		assertEquals("[getRelayname] not correct!", expected, actual);
+	}
+
+	@Test
+	public void testGetPosition() {
+		Position actual = sut.getPosition();
+
+		Position expected = Position.UNKNOWN;
+		assertEquals("[getPosition] not correct!", expected, actual);
+	}
+
+	@Test
+	public void testGetStatus() {
+		String actual = sut.getStatus();
+
+		String expected = "";
+		assertEquals("[getStatus] not correct!", expected, actual);
+
 	}
 }
