3b3ddbc1149ffa63cd0cab8bd77b65e9d4f328e7
Christian Schmoll <it1f36@retail-sc.com>
2017-02-03 12:33:48 +0100

    #REL-214 - add member.

---
+++ relayD-domain-model/src/main/java/com/relayd/Member.java
@@ -0,0 +1,118 @@
+package com.relayd;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import com.relayd.attributes.Email;
+import com.relayd.attributes.Forename;
+import com.relayd.attributes.Surename;
+
+/**
+ * @author schmollc (Christian@relayd.de)
+ * @since 02.02.2017
+ *
+ */
+public class Member implements Serializable {
+	private static final long serialVersionUID = -1657222726828950264L;
+
+	private UUID uuidPerson;
+	private Forename forename = Forename.newInstance();
+	private Surename surename = Surename.newInstance();
+	private Email email = Email.newInstance();
+
+	private Member() {
+	}
+
+	private Member(Forename aForename, Surename aSurename, Email anEmail, UUID anUuid) {
+		forename = aForename;
+		surename = aSurename;
+		email = anEmail;
+		uuidPerson = anUuid;
+	}
+
+	public static Member newInstance() {
+		return MemberNullObject.instance();
+	}
+
+	public static Member newInstance(Person person) {
+		if (person == null) {
+			return MemberNullObject.instance();
+		}
+		return new Member(person.getForename(), person.getSurename(), person.getEmail(), person.getUuid());
+	}
+
+	public boolean isEmpty() {
+		return false;
+	}
+
+	public UUID getUuidPerson() {
+		return uuidPerson;
+	}
+
+	public Forename getForename() {
+		return forename;
+	}
+
+	public Surename getSurename() {
+		return surename;
+	}
+
+	public Email getEmail() {
+		return email;
+	}
+
+	@Override
+	public String toString() {
+		return getForename() + " " + getSurename();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((uuidPerson == null) ? 0 : uuidPerson.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Member other = (Member) obj;
+		if (uuidPerson == null) {
+			if (other.uuidPerson != null) {
+				return false;
+			}
+		} else if (!uuidPerson.equals(other.uuidPerson)) {
+			return false;
+		}
+		return true;
+	}
+
+	static final class MemberNullObject extends Member {
+		private static final long serialVersionUID = 6577776791000840413L;
+
+		private static final MemberNullObject SINGLETON = new MemberNullObject();
+
+		private static MemberNullObject instance() {
+			return SINGLETON;
+		}
+
+		@Override
+		public boolean isEmpty() {
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "";
+		}
+	}
+}
---
+++ relayD-domain-model/src/test/java/com/relayd/MemberTest.java
@@ -0,0 +1,283 @@
+package com.relayd;
+
+import static org.junit.Assert.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.relayd.Member.MemberNullObject;
+import com.relayd.attributes.Email;
+import com.relayd.attributes.Forename;
+import com.relayd.attributes.Surename;
+
+/**
+ * Verlasse dich auf nichts.
+ *  - Miyamoto Musashi
+ *
+ * @author schmollc (Christian@relayd.de)
+ * @since 02.02.2017
+ *
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class MemberTest {
+
+	@Test
+	public void testIsSerializable() {
+		Member sut = Member.newInstance();
+
+		@SuppressWarnings("cast")
+		boolean result = sut instanceof Serializable;
+
+		assertTrue("Class not Serializable!", result);
+	}
+
+	@Test
+	public void testCreateInstance() {
+		Member sut = Member.newInstance();
+
+		assertNotNull("Not a valid instance!", sut);
+
+		boolean result = sut.getClass() == MemberNullObject.class;
+
+		assertTrue("Instance is not correct!", result);
+
+	}
+
+	@Test
+	public void testCreateInstance_ForParameterPerson() {
+		Person dummyPerson = Person.newInstance();
+
+		Member sut = Member.newInstance(dummyPerson);
+
+		assertNotNull("Not a valid instance!", sut);
+
+		boolean result = sut.getClass() == Member.class;
+
+		assertTrue("Instance is not correct!", result);
+	}
+
+	@Test
+	public void testCreateInstance_ForNullValue() {
+		Member sut = Member.newInstance(null);
+
+		assertNotNull("Not a valid instance!", sut);
+
+		boolean result = sut.getClass() == MemberNullObject.class;
+
+		assertTrue("Instance is not correct!", result);
+	}
+
+	@Test
+	public void testGetUuidMember() {
+		UUID expected = UUID.randomUUID();
+
+		Person person = Person.newInstance();
+		person.setUuid(expected);
+
+		Member sut = Member.newInstance(person);
+
+		UUID actual = sut.getUuidPerson();
+
+		assertEquals("[uuid] not correct!", expected, actual);
+	}
+
+	@Test
+	public void testGetForename() {
+		Forename expected = Forename.newInstance("Jonas");
+
+		Person person = Person.newInstance();
+		person.setUuid(UUID.randomUUID());
+		person.setForename(expected);
+
+		Member sut = Member.newInstance(person);
+
+		Forename actual = sut.getForename();
+
+		assertEquals("[forename] not correct!", expected, actual);
+	}
+
+	@Test
+	public void testGetSurename() {
+		Surename expected = Surename.newInstance("Justus");
+
+		Person person = Person.newInstance();
+		person.setUuid(UUID.randomUUID());
+		person.setSurename(expected);
+
+		Member sut = Member.newInstance(person);
+
+		Surename actual = sut.getSurename();
+
+		assertEquals("[surename] not correct!", expected, actual);
+	}
+
+	@Test
+	public void testGetEmail() {
+		Email expected = Email.newInstance("Justus.Jonas@canda.com");
+
+		Person person = Person.newInstance();
+		person.setUuid(UUID.randomUUID());
+		person.setEmail(expected);
+
+		Member sut = Member.newInstance(person);
+
+		Email actual = sut.getEmail();
+
+		assertEquals("[email] not correct!", expected, actual);
+	}
+
+	@Test
+	public void testIsEmpty_ForValueEmpty() {
+		Member sut = Member.newInstance();
+
+		boolean condition = sut.isEmpty();
+
+		assertTrue("[condition] for isEmpty is not correct!", condition);
+	}
+
+	@Test
+	public void testIsEmpty_ForValueFilled() {
+		Person dummyPerson = Person.newInstance();
+		dummyPerson.setUuid(UUID.randomUUID());
+		dummyPerson.setForename(Forename.newInstance("Justus"));
+		dummyPerson.setSurename(Surename.newInstance("Jonas"));
+		dummyPerson.setEmail(Email.newInstance("Justus.Jonas@canda.com"));
+
+		Member sut = Member.newInstance(dummyPerson);
+
+		boolean condition = sut.isEmpty();
+
+		assertFalse("[result] for isEmpty is not correct!", condition);
+	}
+
+	@Test
+	public void testToString_ForEmptyForeAndSurename() {
+		Member sut = Member.newInstance();
+
+		String actual = sut.toString();
+
+		assertEquals("[toString] not correct!", "", actual);
+	}
+
+	@Test
+	public void testToString_ForFilledForeNameSurenameAndEmail() {
+		Person person = Person.newInstance();
+		person.setUuid(UUID.randomUUID());
+		person.setForename(Forename.newInstance("Justus"));
+		person.setSurename(Surename.newInstance("Jonas"));
+		person.setEmail(Email.newInstance("Justus.Jonas@canda.com"));
+
+		Member sut = Member.newInstance(person);
+
+		String actual = sut.toString();
+
+		assertEquals("[toString] not correct!", "Justus Jonas", actual);
+	}
+
+	@Test
+	public void testHashCode() {
+		Person person = Person.newInstance();
+		person.setUuid(UUID.fromString("5697d710-8967-4b2d-9ab2-8fc50ddc6138"));
+
+		Member sut = Member.newInstance(person);
+		int hashCode = sut.hashCode();
+
+		assertEquals(1218343647, hashCode);
+
+		Member sutWithoutUUID = Member.newInstance();
+
+		hashCode = sutWithoutUUID.hashCode();
+
+		assertEquals(31, hashCode);
+	}
+
+	@Test
+	public void testEquals_ForMyself() {
+		Member sut = Member.newInstance();
+
+		boolean result = sut.equals(sut);
+
+		assertTrue(result);
+	}
+
+	@Test
+	public void testEquals_ForNull() {
+		Member sut = Member.newInstance();
+
+		boolean result = sut.equals(null);
+
+		assertFalse(result);
+	}
+
+	@Test
+	public void testEquals_ForNotCompatibleClass() {
+		Member sut = Member.newInstance();
+
+		boolean result = sut.equals(new String());
+
+		assertFalse(result);
+	}
+
+	@Test
+	public void testEquals_ForIdIsNull() {
+		Person dummyPerson = Person.newInstance();
+		dummyPerson.setUuid(null);
+
+		Member sut = Member.newInstance(dummyPerson);
+
+		UUID uuidForSecondSut = UUID.randomUUID();
+		dummyPerson.setUuid(uuidForSecondSut);
+		Member secondSut = Member.newInstance(dummyPerson);
+
+		boolean result = sut.equals(secondSut);
+
+		assertFalse(result);
+	}
+
+	@Test
+	public void testEquals_ForBothIdsAreNull() {
+		Person dummyPerson = Person.newInstance();
+		dummyPerson.setUuid(null);
+
+		Member sut = Member.newInstance(dummyPerson);
+		Member secondSut = Member.newInstance(dummyPerson);
+
+		boolean result = sut.equals(secondSut);
+
+		assertTrue(result);
+	}
+
+	@Test
+	public void testEquals_ForTwoDiffrentIds() {
+		UUID uuidForSut = UUID.randomUUID();
+		Person dummyPerson = Person.newInstance();
+		dummyPerson.setUuid(uuidForSut);
+
+		Member sut = Member.newInstance(dummyPerson);
+
+		UUID uuidForSecondSut = UUID.randomUUID();
+		dummyPerson.setUuid(uuidForSecondSut);
+		Member secondSut = Member.newInstance(dummyPerson);
+
+		boolean result = sut.equals(secondSut);
+
+		assertFalse(result);
+	}
+
+	@Test
+	public void testEquals_ForSameIds() {
+		Person dummyPerson = Person.newInstance();
+		dummyPerson.setUuid(UUID.randomUUID());
+
+		Member sut = Member.newInstance(dummyPerson);
+		Member secondSut = Member.newInstance(dummyPerson);
+
+		boolean result = sut.equals(secondSut);
+
+		assertTrue(result);
+	}
+}
