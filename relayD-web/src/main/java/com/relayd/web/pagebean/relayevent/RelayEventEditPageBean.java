136742b09ddbb86e3e3aebe396d3c07d944a263e
Christian Schmoll <it1f36@retail-sc.com>
2017-02-08 15:59:58 +0100

    #REL-224 - create edit class.

---
+++ relayD-web/src/main/java/com/relayd/web/pagebean/relayevent/RelayEventEditPageBean.java
@@ -0,0 +1,23 @@
+package com.relayd.web.pagebean.relayevent;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * @author schmollc
+ * @since 08.02.2017
+ *
+ */
+@ManagedBean(name = "relayEventEditPageBean")
+@SessionScoped
+public class RelayEventEditPageBean implements Serializable {
+	private static final long serialVersionUID = 453304395884163605L;
+
+	public void openDialog() {
+	}
+
+	public void closeDialog() {
+	}
+}
---
+++ relayD-web/src/test/java/com/relayd/web/pagebean/relayevent/RelayEventEditPageBeanTest.java
@@ -0,0 +1,44 @@
+package com.relayd.web.pagebean.relayevent;
+
+import static org.junit.Assert.*;
+
+import java.io.Serializable;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Die Sorge, was andere wohl von einem denken, verfliegt, wenn man merkt, wie selten sie an einen denken,
+ *  - David Foster Wallace
+ *
+ *
+ *
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(MockitoJUnitRunner.class)
+public class RelayEventEditPageBeanTest {
+
+	@Spy
+	private RelayEventEditPageBean sut;
+
+	@Before
+	public void setUp() {
+		doNothing().when(sut).openDialog();
+		doNothing().when(sut).closeDialog();
+	}
+
+	@Test
+	public void testIsSerializable() {
+		@SuppressWarnings("cast")
+		boolean condition = sut instanceof Serializable;
+
+		assertTrue("Klasse nicht Serializable!", condition);
+	}
+}
