2ef9c70f9333f6f25aeaeae3ad24459d9fed7155
Christian Schmoll <it1f36@retail-sc.com>
2017-04-06 16:10:59 +0200

    #REL-244 - Use no longer the RelayEvent.duesseldorf().

--- relayD-domain-model/src/main/java/com/relayd/ejb/orm/memory/RelayEventGatewayMemory.java
+++ relayD-domain-model/src/main/java/com/relayd/ejb/orm/memory/RelayEventGatewayMemory.java
@@ -1,12 +1,17 @@
 package com.relayd.ejb.orm.memory;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import com.relayd.ParticipantBuilder;
 import com.relayd.RelayEvent;
+import com.relayd.attributes.EventDay;
+import com.relayd.attributes.Eventname;
 import com.relayd.ejb.RelayEventGateway;
 
 /**
@@ -29,7 +34,14 @@ public class RelayEventGatewayMemory implements RelayEventGateway {
 	}
 
 	private RelayEvent createEventForDuesseldorfMarathon() {
-		RelayEvent relayEvent = RelayEvent.duesseldorf();
+		Eventname eventName = Eventname.newInstance("Metro Group Marathon Düsseldorf");
+		EventDay eventDay = EventDay.newInstance(LocalDate.of(2017, Month.APRIL, 30));
+
+		RelayEvent relayEvent = RelayEvent.newInstance(eventName, eventDay);
+
+		relayEvent.addParticipant(new ParticipantBuilder().withForename("Justus").withSurename("Jonas").build());
+		relayEvent.addParticipant(new ParticipantBuilder().withForename("Peter").withSurename("Shaw").build());
+
 		return relayEvent;
 	}
 
