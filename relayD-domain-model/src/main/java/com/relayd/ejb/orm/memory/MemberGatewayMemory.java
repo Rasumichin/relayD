242597c264f5f91f5f37b1c78a10342b1f1fa5f4
Christian Schmoll <it1f36@retail-sc.com>
2017-06-04 16:44:37 +0200

    REL-273 - edit the duration is possible for MEMORY mode.

--- relayD-domain-model/src/main/java/com/relayd/ejb/orm/memory/MemberGatewayMemory.java
+++ relayD-domain-model/src/main/java/com/relayd/ejb/orm/memory/MemberGatewayMemory.java
@@ -3,6 +3,7 @@ package com.relayd.ejb.orm.memory;
 import java.util.UUID;
 
 import com.relayd.Member;
+import com.relayd.Relay;
 import com.relayd.ejb.MemberGateway;
 
 /**
@@ -14,6 +15,13 @@ public class MemberGatewayMemory implements MemberGateway {
 
 	@Override
 	public Member get(UUID uuid) {
+		for (Relay eachRelay : MemorySingleton.getInstance().getRelays().values()) {
+			for (Member eachMember : eachRelay.getMembers()) {
+				if (uuid.equals(eachMember.getUuid())) {
+					return eachMember;
+				}
+			}
+		}
 		return MemorySingleton.getInstance().getMembers().get(uuid);
 	}
 
