c5be46efe2a873579218ce24dbd160a775745830
Christian Schmoll <it1f36@retail-sc.com>
2017-07-25 18:58:16 +0200

    REL-279 - Remove ToDo.

--- relayD-web/src/main/java/com/relayd/web/local/I18N.java
+++ relayD-web/src/main/java/com/relayd/web/local/I18N.java
@@ -5,19 +5,25 @@ package com.relayd.web.local;
  * @since 05.04.2017
  *
  */
+// TODO - REL-290 - Meldungen aus I18N abgleichen.
 public final class I18N {
 	// @formatter:off
-	public static final String NOT_POSSIBLE 		= "Not Possible!";
-	public static final String SELECT_A_ROW 		= "Please select one row!";
-	public static final String SAVED 				= "Saved!";
-	public static final String ADDED 				= "Added!";
-	public static final String CANCELED				= "Canceled!";
-	public static final String EMAIL				= "Email";
-	public static final String EMAIL_NOT_UNIQUE		= "Email not uniqe!";
-	public static final String USED_GATEWAY			= "Used Gateway:";
-	public static final String SUCCESS 				= "Success";
-	public static final String REMOVE 				= "Remove";
-	public static final String SELECT_A_ROW_PERSON 	= "Please select one person row!";
-	public static final String SELECT_A_ROW_RELAY 	= "Please select one relay row!";
+	public static final String ONLY_FOR_MEMBER_ROW_POSSIBLE = "Only for Member Row possible!";
+	public static final String NOT_POSSIBLE 				= "Not Possible!";
+	public static final String SELECT_A_ROW 				= "Please select one row!";
+	public static final String SELECT_A_ROW_2				= "Please select a Row!";
+	public static final String SAVED 						= "Saved!";
+	public static final String ADDED 						= "Added!";
+	public static final String CANCELED						= "Canceled!";
+	public static final String EMAIL						= "Email";
+	public static final String EMAIL_NOT_UNIQUE				= "Email not uniqe!";
+	public static final String USED_GATEWAY					= "Used Gateway:";
+	public static final String SUCCESS 						= "Success";
+	public static final String REMOVE 						= "Remove";
+	public static final String SELECT_A_ROW_PERSON 			= "Please select one person row!";
+	public static final String SELECT_A_ROW_RELAY 			= "Please select one relay row!";
+	public static final String SELECT_A_PERSON				= "Please select a Person!";
+	public static final String SELECT_A_SINGLE_PERSON		= "Please select a single Person!";
+	public static final String NOT_IMPLEMENTD_YET			= "Not implemented yet!";
 	// @formatter:on
 }
