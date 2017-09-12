86dbb7318a0abdb5f97534b0a7b817d6dcb42be8
Christian Schmoll <it1f36@retail-sc.com>
2017-09-12 19:26:40 +0200

    REL-267 - create IT Test.

--- relayD-entity-model/scripts/participant-db-ddl.sql
+++ relayD-entity-model/scripts/participant-db-ddl.sql
@@ -7,10 +7,10 @@ DROP TABLE participant;
 -- Create Table participant
 -- ------------------------------------------------
 CREATE TABLE participant (
-	id								CHAR(36)		NOT NULL			COMMENT 'UUID as string representation.',
-	personId						CHAR(36)		NOT NULL			COMMENT 'UUID of the referenced person, string representation.',
-	relayEventId		 			CHAR(36) 		NOT NULL			COMMENT 'UUID of the referenced relayEvent, string representation.',
-	info							VARCHAR(1024) 						COMMENT on COLUMN participant.info is 'A comment for a Participant.' -- COMMENT geht nicht
+	id							CHAR(36)		NOT NULL			COMMENT 'UUID as string representation.',
+	personId					CHAR(36)		NOT NULL			COMMENT 'UUID of the referenced person, string representation.',
+	relayEventId		 		CHAR(36) 		NOT NULL			COMMENT 'UUID of the referenced relayEvent, string representation.',
+	info						VARCHAR(1024) 						COMMENT 'A comment for a Participant.', -- COMMENT geht nicht
 	CONSTRAINT fk_participant_person
 		FOREIGN KEY (personId) REFERENCES person (id)
 		ON DELETE RESTRICT
