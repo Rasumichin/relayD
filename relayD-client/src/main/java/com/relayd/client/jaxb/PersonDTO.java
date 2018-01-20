244872e20d6def93d06efb460a3c05e776b9fd8e
Erik Lotz <elo@jsmithy.de>
2018-01-20 16:02:09 +0100

    REL-341 Add getters and properties for 'PersonDTO'.

--- relayD-client/src/main/java/com/relayd/client/jaxb/PersonDTO.java
+++ relayD-client/src/main/java/com/relayd/client/jaxb/PersonDTO.java
@@ -1,6 +1,6 @@
 package com.relayd.client.jaxb;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.*;
 
 /**
  * @author Rasumichin (Erik@relayd.de)
@@ -9,4 +9,46 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement(name = "person")
 public class PersonDTO {
+	private String id;
+	private String forename;
+	private String surename;
+	private Integer yearOfBirth;
+	private String shirtSize;
+	private String eMail;
+	private String comment;
+
+	@XmlElement
+	public String getId() {
+		return id;
+	}
+
+	@XmlElement
+	public String getForename() {
+		return forename;
+	}
+
+	@XmlElement
+	public String getSurename() {
+		return surename;
+	}
+
+	@XmlElement
+	public Integer getYearOfBirth() {
+		return yearOfBirth;
+	}
+
+	@XmlElement
+	public String getShirtSize() {
+		return shirtSize;
+	}
+
+	@XmlElement
+	public String getEmail() {
+		return eMail;
+	}
+
+	@XmlElement
+	public String getComment() {
+		return comment;
+	}
 }
--- relayD-client/src/test/java/com/relayd/client/jaxb/PersonDTOTest.java
+++ relayD-client/src/test/java/com/relayd/client/jaxb/PersonDTOTest.java
@@ -16,4 +16,53 @@ public class PersonDTOTest {
 	public void testNew() {
 		assertNotNull("Instance creation was not correct!", sut);
 	}
+	
+	@Test
+	public void testGetId() {
+		String id = sut.getId();
+		
+		assertNull("Initial value of 'id' is not correct!", id);
+	}
+
+	@Test
+	public void testGetForename() {
+		String forename = sut.getForename();
+		
+		assertNull("Initial value of 'forename' is not correct!", forename);
+	}
+
+	@Test
+	public void testGetSurename() {
+		String surename = sut.getSurename();
+		
+		assertNull("Initial value of 'surename' is not correct!", surename);
+	}
+
+	@Test
+	public void testGetYearOfBirth() {
+		Integer yearOfBirth = sut.getYearOfBirth();
+		
+		assertNull("Initial value of 'yearOfBirth' is not correct!", yearOfBirth);
+	}
+
+	@Test
+	public void testGetShirtSize() {
+		String shirtSize = sut.getShirtSize();
+		
+		assertNull("Initial value of 'shirtSize' is not correct!", shirtSize);
+	}
+
+	@Test
+	public void testGetEmail() {
+		String eMail = sut.getEmail();
+		
+		assertNull("Initial value of 'eMail' is not correct!", eMail);
+	}
+
+	@Test
+	public void testGetComment() {
+		String comment = sut.getComment();
+		
+		assertNull("Initial value of 'comment' is not correct!", comment);
+	}
 }
